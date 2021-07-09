5c4cad9727d6179642a0c88e24dc98b3f5de67b1
Mingyang-Li <mingyangli118@gmail.com>
2021-07-09 16:14:04 +1200

    pseudocode 1st draft

--- src/getEraPayout.ts
+++ src/getEraPayout.ts
@@ -10,13 +10,34 @@ export async function getEraPayout(eraNumber: number, validatorId: string) {
     const blockHash: BlockHash = await api.rpc.chain.getBlockHash(parseInt(validatorId));
 
     // Checking payouts from this particulat era
-    const allEraPayouts = await api.derive.accounts.accountId("0xc9845b0733e6319ab537d6801dcaba46110003e6dffaa4a2cb5f0a78ba872dd3");
+    // const allEraPayouts = await api.query.staking.erasStakers;
+    const allEraPayouts: any= [{ payoutId: 1, isClaimed: false, claimedBy: "me" }];
 
     // Checking which payouts are claimed and unclaimed
+    const payoutsByClaimedStatus = allEraPayouts.map((p: any) => {
+        {
+            p.payoutId,
+            p.isClaimed,
+            p // reference to originl payout data for future use
+        }
+    })
 
     // For all claimed payouts, find out who claimed them.
+    const getClaimedPayoutsByClaimers = () => {
+        let arr: any = [];
+        payoutsByClaimedStatus.forEach((p: any) => {
+            if (p.isClaimed) {
+                arr = [
+                    ...
+                    [p.payoutId, p.claimedBy, p]
+                ]
+            }
+        })
+    }
+
+    const claimedPayoutByClaimers = getClaimedPayoutsByClaimers();
 
     // For all payouts claimed by the system, find out which era did each payout happen within
 
-    return allEraPayouts;
+    return { allEraPayouts, payoutsByClaimedStatus, claimedPayoutByClaimers };
 }
